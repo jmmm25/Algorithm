3880ca05cfaa669fa4c998847f1c746cff4cf5c1
김정민 <jmkim9302@gmail.com>
2021-04-08 07:49:19 +0900

    update hackerrank algorithm solutions

---
+++ HackerRank/Interview Preparation Kit/Warm-up Challenges/Counting Valleys/main.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'countingValleys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER steps
+ *  2. STRING path
+ */
+
+int countingValleys(int steps, string path) {
+    int ans = 0;
+    int step = 0;
+    bool isMinus = false;
+
+    for (auto i : path) {
+        if (i == 'U') {
+            step++;
+            if (isMinus && step == 0) {
+                ans++;
+            }
+        } else {
+            step--;
+        }
+
+        if (step < 0) {
+            isMinus = true;
+        } else {
+            isMinus = false;
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string steps_temp;
+    getline(cin, steps_temp);
+
+    int steps = stoi(ltrim(rtrim(steps_temp)));
+
+    string path;
+    getline(cin, path);
+
+    int steps;
+    string path;
+    int result = countingValleys(steps, path);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(s.begin(),
+            find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace))));
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end());
+
+    return s;
+}
---
+++ HackerRank/Interview Preparation Kit/Warm-up Challenges/Jumping on the Clouds/main.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+int jumpingOnClouds(vector<int> c) {
+    int count = 0;
+    int i = 0;
+    while (i + 1 < c.size()) {
+        if (c[i + 2] != 1) {
+            count++;
+            i += 2;
+        } else if (c[i + 1] == 0) {
+            count++;
+            i++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string c_temp_temp;
+    getline(cin, c_temp_temp);
+
+    vector<string> c_temp = split_string(c_temp_temp);
+
+    vector<int> c(n);
+
+    for (int i = 0; i < n; i++) {
+        int c_item = stoi(c_temp[i]);
+
+        c[i] = c_item;
+    }
+
+    int result = jumpingOnClouds(c);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(
+        input_string.begin(), input_string.end(),
+        [](const char &x, const char &y) { return x == y and x == ' '; });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(
+        input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
